f53c75dcc9cd4a7f03af82177b008f284ff1ab55
me <k89jy@naver.com>
2020-07-04 12:04:16 +0900

    use function overloading

--- DScode/List.cpp
+++ DScode/List.cpp
@@ -2,6 +2,8 @@
 class MyList{
     // char, string, integer, boolean, .....
     // make fuction parameter that takes all types
+
+    //quick solution: use function overloading 
 }
 
 void addElement(){
