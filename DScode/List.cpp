c7436fcd084e0c3f13cb57f9c38b00680831bad9
me <k89jy@naver.com>
2020-07-04 12:07:13 +0900

    document c++ basic functionallty

--- DScode/List.cpp
+++ DScode/List.cpp
@@ -1,11 +1,14 @@
 
-class MyList{
+
     // char, string, integer, boolean, .....
     // make fuction parameter that takes all types
 
     //quick solution: use function overloading 
-}
 
-void addElement(){
-    MyList.add()
-}
+    //notion1: call by value
+    // def: only the variable value is plugged in to the parameter
+
+    //notiion2: call by reference
+    // def: the variable it self is plugged in to the parameter
+
+
