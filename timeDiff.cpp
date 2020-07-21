d3c18aeee4bc878394893838e4ce0d371ed4044b
me <k89jy@naver.com>
2020-07-21 21:00:27 +0900

    measure time diff numeric & logical op

---
+++ timeDiff.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ctime>
+#include <chrono>
+#include <sys/time.h>
+
+using namespace std;
+
+
+auto times = chrono::system_clock::now();
+
+auto millis = chrono::duration_cast<chrono::milliseconds>(times.time_since_epoch());
+
+int main(){
+
+
+	cout << millis.count() << endl;
+
+	return 0;
+
+}
