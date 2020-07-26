c1e15259e47a36f737331d542f086899e43044c7
me <k89jy@naver.com>
2020-07-26 14:48:23 +0900

    Strings and pointers

---
+++ lists/str_list.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+	//put strings in a list
+
+	string str_list[5];
+
+	str_list[0] = "abc";
+
+	str_list[1] = "def";
+
+	cout << str_list[0] << endl;
+
+	return 0;
+}
